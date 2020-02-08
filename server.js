4b670376ddc963ef86454ea9d22336f4cad30b42
Emily Yu <bluerainmango@gmail.com>
2020-02-07 22:59:45 -0800

    added public folder, handlebars

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const exphbs = require("express-handlebars");
 const path = require("path");
 
 const burgerRouter = require("./controllers/burgers_controller");
@@ -11,7 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use("/api/burgers", burgerRouter);
+app.use("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, err => {
   if (err) throw err;
