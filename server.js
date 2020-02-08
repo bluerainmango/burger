b2bdc72cdad0e24ac512e7c30b20e906cf24e20a
Emily Yu <bluerainmango@gmail.com>
2020-02-08 02:34:50 -0800

    added partials: burger-block

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const path = require("path");
 
 const burgerRouter = require("./controllers/burgers_controller");
+const { Burger } = require("./models/burger");
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,8 +19,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use("/api/burgers", burgerRouter);
-app.use("/", (req, res) => {
-  res.render("index");
+app.use("/", async (req, res) => {
+  const burgers = await Burger.selectAll();
+  res.render("index", { burgers });
 });
 
 app.listen(PORT, err => {
