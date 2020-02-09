1340b40a48c77976db4f0a23aa1be42552a70735
Emily Yu <bluerainmango@gmail.com>
2020-02-09 15:12:07 -0800

    changed connection to pool

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,33 @@
 const mysql = require("mysql");
 const { promisify } = require("util");
 
-console.log("env", process.env.NODE_ENV);
-
-let connection;
+let pool;
 
 if (process.env.NODE_ENV === "development") {
   // When development mode, connect to local DB
-  connection = mysql.createConnection({
+  pool = mysql.createPool({
+    connectionLimit: 10,
     host: "localhost",
     user: process.env.DEV_DB_USER,
     password: process.env.DEV_DB_PWD,
-    database: "burgers_db"
+    database: "burgers_db",
+    acquireTimeout: 10000
   });
 } else {
   // When production mode, connect to remote DB
-  connection = mysql.createConnection(process.env.DATABASE_URL);
+  pool = mysql.createPool(process.env.DATABASE_URL);
 }
 
-connection.connect(err => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.log(`🚨 Cannot connect to DB! : ${err}`);
+    connection.destroy();
     process.exit(1);
   }
   console.log("Connected to DB...");
 });
 
 // Promisify connection.query method to use with async/await later
-connection.proQuery = promisify(connection.query).bind(connection);
+pool.proQuery = promisify(pool.query).bind(pool);
 
-module.exports = connection;
+module.exports = pool;
--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,17 @@
-const connection = require("./connection");
+const pool = require("./connection");
 
 exports.orm = {
   selectAll: (table, orderCol = "id") => {
     // Order by 'changed_at' or 'id'
     const q = "SELECT * FROM ?? ORDER BY ??";
-    return connection.proQuery(q, [table, orderCol]);
+    return pool.proQuery(q, [table, orderCol]);
   },
   insertOne: (table, postObj) => {
     const q = "INSERT INTO ?? SET ?";
-    return connection.proQuery(q, [table, postObj]);
+    return pool.proQuery(q, [table, postObj]);
   },
   updateOne: (table, postObj, whereCol, whereVal) => {
     const q = "UPDATE ?? SET ? WHERE ?? = ?";
-    return connection.proQuery(q, [table, postObj, whereCol, whereVal]);
+    return pool.proQuery(q, [table, postObj, whereCol, whereVal]);
   }
 };
